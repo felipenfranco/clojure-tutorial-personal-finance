eeb5c395713215ad70b3d0e6708db0d97c343dc2
Felipe Franco <felipenfranco@gmail.com>
2019-11-04 14:05:51 -0300

    Tag unit/integration tests
    This way we can run them separately using Midje

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ You will need [Leiningen][] 2.0.0 or above installed.
 
 [leiningen]: https://github.com/technomancy/leiningen
 
+## Running tests
+
+    lein midje :filter unit
+
+    lein midje :filter integration
+
 ## Running
 
 To start a web server for the application, run:
--- test/unit/finance/handler_test.clj
+++ test/unit/finance/handler_test.clj
@@ -3,21 +3,21 @@
             [ring.mock.request :as mock]
             [finance.handler :refer :all]))
 
-(facts "Root URL is 'Hello World'"
+(facts "Root URL is 'Hello World'" :unit
        (let [response (app (mock/request :get "/"))]
          (fact "status code is 200"
                (:status response) => 200)
          (fact "response body is 'Hello World'"
                (:body response) => "Hello World")))
 
-(facts "Invalid route does not exist"
+(facts "Invalid route does not exist" :unit
        (let [response (app (mock/request :get "/invalid-route"))]
          (fact "status code is 404"
                (:status response) => 404)
          (fact "response body is 'Not Found'"
                (:body response) => "Not Found")))
 
-(facts "Initial balance is 0"
+(facts "Initial balance is 0" :unit
        (let [response (app (mock/request :get "/balance"))]
          (fact "status code is 200"
                (:status response) => 200)
