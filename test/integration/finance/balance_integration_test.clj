badeae606d1c04c65c2581091168d1011b0cafec
Felipe Franco <felipenfranco@gmail.com>
2019-11-04 14:18:07 -0300

    Move start/stop server from test body to setup/teadown phases

--- test/integration/finance/balance_integration_test.clj
+++ test/integration/finance/balance_integration_test.clj
@@ -13,7 +13,7 @@
 (defn stop-server []
   (.stop @server))
 
-(fact "Initial balance is 0" :integration
-      (start-server 3001)
-      (:body (http/get "http://localhost:3001/balance")) => "0"
-      (stop-server))
+(against-background [(before :facts (start-server 3001))
+                     (after :facts (stop-server))]
+ (fact "Initial balance is 0" :integration
+       (:body (http/get "http://localhost:3001/balance")) => "0"))
